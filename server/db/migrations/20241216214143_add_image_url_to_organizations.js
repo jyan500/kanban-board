a1c20b67aa946afa18cb5fa520080cb1feebec76
jyan500 <jyan500@gmail.com>
2024-12-16 13:42:47 -0800

    added migrations for image urls on both users and organizations

---
+++ server/db/migrations/20241216214143_add_image_url_to_organizations.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+	return knex.schema.alterTable("organizations", function(table){
+		table.string("image_url").nullable()
+	}) 
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+	return knex.schema.alterTable("organizations", function(table){
+		table.dropColumn("image_url")
+	}) 
+};
