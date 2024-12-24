149ee29ccc4dbb5eb0f714fd5bd3ef1563e4cae6
jyan500 <jyan500@gmail.com>
2024-12-24 15:51:28 -0800

    fixed lag after notification updating by removing the providesTags from the pollNotifications service, I think by providing tags for pollNotifications, that caused the invalidationProcess to be slower as it needed to wait for a successful re-run of the pollNotification

--- client/src/components/NotificationDropdown.tsx
+++ client/src/components/NotificationDropdown.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react"
 import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks"
 import { Dropdown } from "./Dropdown" 
 import { 
+	useUpdateNotificationMutation,
 	useBulkEditNotificationsMutation, 
 } from "../services/private/notification"
 import { addToast } from "../slices/toastSlice"
@@ -29,6 +30,7 @@ const ReadOnlyDropdownOption = ({children}: ReadOnlyDropdownOptionProps) => {
 export const NotificationDropdown = React.forwardRef<HTMLDivElement, Props>(({closeDropdown, notifications}: Props, ref) => {
 	const dispatch = useAppDispatch()
     const [ bulkEditNotifications, { error: bulkEditNotificationsError, isLoading: isBulkEditNotificationsLoading }] = useBulkEditNotificationsMutation()
+    const [ updateNotification, {error: updateNotificationError, isLoading: isUpdateNotificationLoading}] = useUpdateNotificationMutation()
 	const markMessagesRead = async (messages: Array<Notification>) => {
 		if (messages?.length){
 			try {
@@ -45,22 +47,40 @@ export const NotificationDropdown = React.forwardRef<HTMLDivElement, Props>(({cl
 		}		
 	}
 
+	const markMessageRead = async (message: Notification) => {
+		try {
+			await updateNotification({isRead: true, id: message.id}).unwrap()
+		}
+		catch {
+			dispatch(addToast({
+				id: uuidv4(),
+				message: "Failed to mark notification as read.",
+				animationType: "animation-in",
+				type: "failure"
+			}))	
+		}
+	}
+
 	return (
 		<Dropdown className = {"!tw-w-96"} ref = {ref}>
 			<ReadOnlyDropdownOption
 			>
 				<p className = "tw-font-bold">Notifications: </p>
-				<button onClick={() => {
+				<button onClick={async () => {
 					const unreadMessages = notifications?.filter(n => !n.isRead)
 					if (unreadMessages?.length){
-						markMessagesRead(unreadMessages)
+						await markMessagesRead(unreadMessages)
 					}
 				}} className = "button --secondary">Mark as Read</button>
 			</ReadOnlyDropdownOption>
 			{!notifications?.length ? <ReadOnlyDropdownOption><p>No Notifications Found</p></ReadOnlyDropdownOption> : null}
 			<ul>
 				{notifications?.map((notification) => (
-					<Link to = {notification.objectLink}>
+					<Link onClick={async () => {
+						if (!notification.isRead){
+							await markMessageRead(notification)}
+						}
+					} to = {notification.objectLink}>
 						<li
 							key={`notification-${notification.id}`}
 							className={`${!notification.isRead ? "tw-bg-gray-50" : ""} tw-block hover:tw-bg-gray-50 tw-px-4 tw-py-2 tw-text-sm tw-text-gray-700 tw-hover:bg-gray-100 tw-hover:text-gray-900`}
