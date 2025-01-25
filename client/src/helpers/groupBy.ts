f32f3736f1cdea87f50291f6055d07ce98dd0407
jyan500 <jyan500@gmail.com>
2025-01-24 18:27:11 -0800

    added group by function to group the tickets by their group by option and also status id

--- client/src/helpers/groupBy.ts
+++ client/src/helpers/groupBy.ts
@@ -2,10 +2,10 @@ import { GROUP_BY_OPTIONS } from "./constants"
 import { Ticket, Status, GroupByOptionsKey } from "../types/common"
 
 type GroupByModifier = {
-	[key in GroupByOptionsKey]: any
+	[key in GroupByOptionsKey]: (tickets: Array<Ticket>) => Record<string, Array<Ticket>>
 }
 
-const getGroupByModifier: GroupByModifier = {
+const groupByModifierMap: GroupByModifier = {
 	"NONE": (tickets: Array<Ticket>) => {
 		return {"None": tickets}
 	},
@@ -20,11 +20,31 @@ const getGroupByModifier: GroupByModifier = {
 			return acc
 		}, {})
 	},
-	"EPIC": () => {
-		return []
+	"EPIC": (tickets: Array<Ticket>) => {
+		return tickets.reduce((acc: Record<string, Array<Ticket>>, ticket: Ticket) => {
+			ticket.epicParentTickets?.forEach((epic: {id: number, name: string}) => {
+				if (epic.id in acc){
+					acc[epic.id].push(ticket)
+				}
+				else {
+					acc[epic.id] = [ticket]
+				}
+			})	
+			return acc
+		}, {})
 	},
-	"ASSIGNEE": () => {
-		return []
+	"ASSIGNEE": (tickets: Array<Ticket>) => {
+		return tickets.reduce((acc: Record<string, Array<Ticket>>, ticket: Ticket) => {
+			ticket.assignees?.forEach((userId: number) => {
+				if (userId in acc){
+					acc[userId].push(ticket)
+				}	
+				else {
+					acc[userId] = [ticket]
+				}
+			})
+			return acc
+		}, {})
 	},
 	"TICKET_TYPE": (tickets: Array<Ticket>) => {
 		return tickets.reduce((acc: Record<string, Array<Ticket>>, ticket: Ticket) => {
@@ -39,12 +59,49 @@ const getGroupByModifier: GroupByModifier = {
 	}
 }
 
+/* 
+	Based on the group by option, parses the array of tickets into the grouped version that's separated by status
+	Example:
+	if "TICKET_TYPE" is chosen
+	1) creates an object like so based on the group by modifier
+	{
+		ticketTypeId 1: [Array of tickets with ticketTypeId 1],
+		ticketTypeId 2: [Array of tickets with ticketTypeId 2],
+		...
+	}
+	2) Within each array of tickets, it will further group this by status id like so.
+	{	
+		ticketTypeId 1 : {
+			status id1: [Array of tickets with status id 1]
+			status id2: [Array of tickets with status id 2],
+			...
+		},
+		ticketTypeID 2: {
+			status id1: [Array of tickets with status id 1]
+			status id2: [Array of tickets with status id 2],
+			...
+		},
+		...
+	}	
+*/
 export const boardGroupBy = 
 	(
 		option: GroupByOptionsKey, 
 		tickets: Array<Ticket>, 
 		statusesToDisplay: Array<Status>
-	): Array<any> => {
-		return []
+	): Record<string, Record<string, Array<number>>> => {
+		const modifier = groupByModifierMap[option as GroupByOptionsKey]
+		const grouped: Record<string, Array<Ticket>> = modifier(tickets)
+		return Object.keys(grouped).reduce((acc: Record<string, Record<string, Array<number>>>, key: string) => {
+			const groupedTickets = grouped[key]
+			const groupedTicketIdsByStatusMap = statusesToDisplay.reduce((acc: Record<string, Array<number>>, status: Status) => {
+				acc[(status.id.toString()) as string] = groupedTickets.filter((ticket) => ticket.statusId === status.id).map((ticket) => ticket.id)
+				return acc
+			}, {})
+			if (!(key in acc)){
+				acc[(key.toString()) as string] = groupedTicketIdsByStatusMap
+			}
+			return acc
+		}, {})
 }
 
