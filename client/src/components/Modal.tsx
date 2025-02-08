1a27118b34d9319c0cc84ade5958036cc06f14d6
jyan500 <jyan500@gmail.com>
2025-02-07 18:19:36 -0800

    change modal to be variable modal width based on window size rather than fixed

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -10,7 +10,8 @@ import { BoardForm } from "./BoardForm"
 import { BoardStatusModal } from "./primary-modals/BoardStatusModal"
 import { OrganizationStatusModal } from "./primary-modals/OrganizationStatusModal"
 import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks" 
-import { PRIMARY_MODAL_Z_INDEX } from "../helpers/constants"
+import { PRIMARY_MODAL_Z_INDEX, LG_BREAKPOINT } from "../helpers/constants"
+import { useScreenSize } from "../hooks/useScreenSize"
 
 export const modalTypes = {
 	"ADD_TICKET_FORM": AddTicketFormModal,
@@ -22,8 +23,11 @@ export const modalTypes = {
 }
 
 export const modalClassNames = {
-	"ADD_TICKET_FORM": "--l-modal tw-top-[50%]",
-	"EDIT_TICKET_FORM": "--l-modal tw-top-[50%]",
+	// "ADD_TICKET_FORM": "--l-modal tw-top-[50%]",
+	// "EDIT_TICKET_FORM": "--l-modal tw-top-[50%]",
+	// "ORGANIZATION_STATUS_FORM": "--l-modal-height tw-top-[50%]"
+	"ADD_TICKET_FORM": "--l-modal-height tw-top-[50%]",
+	"EDIT_TICKET_FORM": "--l-modal-height tw-top-[50%]",
 	"ORGANIZATION_STATUS_FORM": "--l-modal-height tw-top-[50%]"
 }
 
@@ -34,6 +38,10 @@ export const Modal = () => {
 	const dispatch = useAppDispatch()
 	const { currentModalType, showModal, currentModalProps }  = useAppSelector((state) => state.modal)
 	const ModalContent = modalTypes[currentModalType as keyof typeof modalTypes] as React.FC 
+	const { width, height } = useScreenSize()
+	const style = {
+		width: `${width <= LG_BREAKPOINT ? width-40 : LG_BREAKPOINT }px`
+	}
 
 	// define modal handlers type as the partial subset of all keys of modal types
 	const modalHandlers: PartialKeys<typeof modalTypes> = {
@@ -57,7 +65,7 @@ export const Modal = () => {
 
 	return (
 		<div className = {`${PRIMARY_MODAL_Z_INDEX} overlay ${showModal ? "--visible": "--hidden"}`}>
-			<div className = {`${currentModalType && currentModalType in modalClassNames ? modalClassNames[currentModalType as keyof typeof modalClassNames] : "tw-top-[30%]"} modal-container`}>
+			<div style = {style} className = {`${currentModalType && currentModalType in modalClassNames ? modalClassNames[currentModalType as keyof typeof modalClassNames] : "tw-top-[30%]"} modal-container`}>
 				<button 
 				className = "__modal-container-close --transparent"
 				onClick={
