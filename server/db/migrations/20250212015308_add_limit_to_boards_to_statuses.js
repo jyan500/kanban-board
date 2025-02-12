171b8ca443530e14a057359999f1351ee6885c44
jyan500 <jyan500@gmail.com>
2025-02-11 17:56:50 -0800

    add migration

---
+++ server/db/migrations/20250212015308_add_limit_to_boards_to_statuses.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+	return knex.schema.alterTable("boards_to_statuses", function(table){
+		table.integer("limit").defaultTo(30)
+	}) 
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+	return knex.schema.alterTable("statuses", function(table){
+		table.dropColumn("limit")
+	}) 
+};
