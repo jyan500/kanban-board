fbbb963e22605e0a49f1ba6963eba66721799612
jyan500 <jyan500@gmail.com>
2025-03-16 12:46:16 -0700

    added missing configuration files

---
+++ client/jest.config.js
@@ -0,0 +1,8 @@
+export {};
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+};
---
+++ server/config.js
@@ -0,0 +1,7 @@
+const config = {
+	saltRounds: 10,
+	listPerPage: 10,
+	email: "noreply@kanban.com"
+}
+
+module.exports = config
