2a6989983b2eeeb432a992a4925ed3b2d5931ec4
jyan500 <jyan500@gmail.com>
2025-03-27 14:31:30 -0700

    added board loading

--- client/src/components/page-elements/Indicator.tsx
+++ client/src/components/page-elements/Indicator.tsx
@@ -7,6 +7,6 @@ type Props = {
 
 export const Indicator = ({showIndicator, className}: Props) => {
 	return (
-		<div className = {`${showIndicator ? "tw-visible" : "tw-hidden"} tw-absolute ${className ?? "tw-w-3 tw-h-3 tw-top-0 tw-right-0 tw-bg-red-500"} tw-rounded-full`}></div>
+		<div className = {`${showIndicator ? "tw-visible" : "tw-hidden"} hover:tw-animate-ping tw-absolute ${className ?? "tw-w-3 tw-h-3 tw-top-0 tw-right-0 tw-bg-red-500"} tw-rounded-full`}></div>
 	)	
 }
---
+++ client/src/components/placeholders/BoardPlaceholder.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const BoardPlaceholder = () => {
+	return (
+		<div className = "tw-w-full tw-flex tw-flex-col tw-gap-y-4">
+			<div className = "tw-w-full tw-h-24 tw-bg-gray-200"></div>	
+			<div className = "tw-w-full tw-h-96 tw-bg-gray-200"></div>	
+		</div>
+	)	
+}
