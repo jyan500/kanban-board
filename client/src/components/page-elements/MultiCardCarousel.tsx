3d6bbad7e6b7c925917b82ff512dbe61c0ff94af
jyan500 <jyan500@gmail.com>
2025-04-04 18:49:43 -0700

    added sample images and carousel

---
+++ client/src/components/page-elements/MultiCardCarousel.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { IconContext } from "react-icons"
+import { GrNext as Next, GrPrevious as Previous } from "react-icons/gr";
+import { IconButton } from "../page-elements/IconButton"
+
+interface Props {
+    items: Array<React.ReactNode>
+    itemsPerPage: number
+    itemContainerClassName?: string
+    index?: number
+}
+
+export const MultiCardCarousel = ({ index, items, itemsPerPage, itemContainerClassName }: Props) => {
+    console.log("index: ", index)
+    const [currentIndex, setCurrentIndex] = useState(index ?? 0)
+    console.log("index: ", currentIndex)
+
+    useEffect(() => {
+        if (index && index >= 0 && index <= items.length){
+            setCurrentIndex(index)
+        }
+    }, [index])
+
+    if (itemsPerPage < 1 || itemsPerPage > items.length) {
+        return <div></div>
+    }
+
+    const style = {
+        ...(itemsPerPage > 1 ? {width: `${1/itemsPerPage*100}%`} : {})
+    }
+
+    const handlePrev = () => {
+        /* 
+            the total number of pages = Math.ceil(items.length/itemsPerPage), so 
+            the last index should be total number of pages - 1
+        */
+        const newIndex = currentIndex === 0 ? Math.ceil(items.length/itemsPerPage) - 1 : currentIndex - 1 
+        setCurrentIndex(newIndex)
+    }
+
+    const handleNext = () => {
+        const newIndex = currentIndex === Math.ceil(items.length/itemsPerPage) - 1 ? 0 : currentIndex + 1
+        setCurrentIndex(newIndex)
+    }
+
+    const setPage = (i: number) => {
+        setCurrentIndex(i)
+    }
+
+    return (
+        <div>
+            <div className = "tw-flex tw-justify-center">
+                <div className={`${itemsPerPage === 1 ? "tw-max-w-[800px]" : ""} tw-relative tw-h-full tw-w-full tw-overflow-hidden tw-rounded-lg`}>
+                    {/* 
+                        transition translate X based on the -(currentIndex) * 100. 
+                        it's negative so the animation will start from right to left
+                    */}
+                    <div className="tw-flex tw-transition-transform tw-duration-700 tw-ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                        {items.map((item, i) => (
+                            /* 
+                                Renders all items, the combination of flex-shrink-0 on each flex item, as well as the width being set to the percent each item takes per page
+                                gives the illusion that only a certain amount of items display on each page based on the itemsPerPage.
+                            */
+                            <div key={`carousel_item_${i}`} style = {style} className = {`${itemContainerClassName ?? ""} tw-relative tw-w-full tw-h-full tw-flex-shrink-0 tw-px-2`}>
+                                {item}
+                            </div>
+                        ))}
+                    </div>
+                    {
+                        itemsPerPage < items.length ? (
+                            <>
+                            <button
+                                className="tw-absolute tw-top-1/2 tw-left-4 tw-transform -tw-translate-y-1/2 hover:tw-bg-white hover:tw-opacity-40 tw-bg-white tw-opacity-60 tw-text-gray-800 tw-px-2 tw-py-2 tw-rounded-full tw-cursor-pointer"
+                                onClick={handlePrev}
+                            >
+                                <IconContext.Provider value = {{className: "tw-w-6 tw-h-6"}}>
+                                    <Previous/> 
+                                </IconContext.Provider> 
+                            </button>
+                            <button
+                                className="tw-absolute tw-top-1/2 tw-right-4 tw-transform -tw-translate-y-1/2 hover:tw-bg-white hover:tw-opacity-40 tw-bg-white tw-opacity-60 tw-text-gray-800 tw-px-2 tw-py-2 tw-rounded-full tw-cursor-pointer"
+                                onClick={handleNext}
+                            >
+                                <IconContext.Provider value = {{className: "tw-w-6 tw-h-6"}}>
+                                    <Next/>
+                                </IconContext.Provider> 
+                            </button>
+                            </>
+                        ) : null
+                    }
+                </div>
+            </div>
+            <div className = "tw-flex tw-flex-col tw-justify-center">
+                {
+                    itemsPerPage < items.length ? (
+                        <div className = "tw-p-4 tw-flex tw-items-center tw-justify-center tw-gap-2">
+                            {
+                                Array.from(Array(Math.ceil(items.length/itemsPerPage)), (_, i) => {
+                                    return (
+                                        <button key = {`carousel_pagination_${i}`} onClick={() => setPage(i)}><div className = {`tw-transition tw-w-3 tw-h-3 tw-bg-gray-800 tw-rounded-full ${currentIndex === i ? "tw-p-2" : "tw-bg-opacity-50"}`}></div></button>
+                                    )
+                                })
+                            }   
+                        </div>
+                    ) : null
+                } 
+            </div>
+        </div>
+    )
+}
+
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { useNavigate } from "react-router-dom"
 import { IconBell } from "../components/icons/IconBell"
 import { IconGear } from "../components/icons/IconGear"
@@ -12,60 +12,127 @@ import { Footer } from "../components/page-elements/Footer"
 import { Header } from "../components/landing-page/Header"
 import { REGISTER } from "../helpers/routes"
 import { FADE_ANIMATION	} from "../helpers/constants"
+import { MultiCardCarousel } from "../components/page-elements/MultiCardCarousel"
+import BacklogImage from "../assets/images/landing-page/backlog.png"
+import BulkActionsImage from "../assets/images/landing-page/bulk-actions.png"
+import EpicTicketsImage from "../assets/images/landing-page/epic-tickets.png"
+import GroupByImage from "../assets/images/landing-page/group-by.png"
+import MentionsImage from "../assets/images/landing-page/mentions.png"
+import NotificationsImage from "../assets/images/landing-page/notifications.png"
+import SettingsImage from "../assets/images/landing-page/organization-settings.png"
+import RTEImage from "../assets/images/landing-page/rich-text-editing.png"
 
-const features = [
+interface Feature {
+	id: number
+	title: string
+	description: string
+	icon?: React.ReactNode
+	imageURL: string
+}
+
+interface CarouselElement {
+	id: number
+	title: string
+	imageURL?: string
+	description?: string
+}
+
+interface CarouselContentProps<T = {}> {
+	data: T
+}
+
+const ImageCarouselContent = ({data}: CarouselContentProps<CarouselElement>) => {
+	return (
+		<>
+	        <img src={data.imageURL} alt={data.title} className="tw-relative tw-object-cover tw-w-full tw-h-full tw-rounded-lg" />
+	        {/* Adds linear gradient starting from mid to bottom of the image to provide more contrast with the white text*/}
+        	<div className="tw-mx-2 tw-rounded-lg tw-absolute tw-inset-0 tw-bg-gradient-to-b tw-from-transparent tw-to-black tw-from-60%">
+		        <p className = "tw-absolute tw-bottom-4 tw-left-4 tw-text-2xl tw-font-bold tw-text-white">{data.title}</p>
+        	</div>
+        </>
+	)
+}
+
+const createImageCarouselElements = (data: Array<CarouselElement>) => {
+	if (data.length){
+		const carouselElements = data.map((element: CarouselElement) => {
+			return <ImageCarouselContent data={element}/>
+		})
+		return carouselElements
+	}
+	return []	
+}
+
+const features: Array<Feature> = [
 	{
+		id: 1,
 		title: "Inline Ticket Editing",
 		description: "Edit tickets with a sleek inline modal featuring rich text support.",
 		icon: <IconTextArea className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-blue-500" />,
+		imageURL: RTEImage,
 	},
 	{
+		id: 2,
 		title: "Ticket Linking & Epics",
 		description: "Organize work efficiently with parent-child and linked issues.",
 		icon: <IconTree className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-green-500" />,
+		imageURL: EpicTicketsImage,
 	},
 	{
+		id: 3,
 		title: "Group by & Drag-and-Drop",
 		description: "Flexible board display with grouping and drag-and-drop statuses.",
 		icon: <IconDragDrop className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-yellow-500" />,
+		imageURL: GroupByImage,
 	},
 	{
+		id: 4,
 		title: "Mentions & Comments",
 		description: "Tag teammates in rich text comments and descriptions.",
 		icon: <IconComment className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-purple-500" />,
+		imageURL: MentionsImage,
 	},
 	{
+		id: 5,
 		title: "Bulk Actions",
 		description: "Apply actions to multiple tickets to optimize your workflow.",
-		icon: <IconBulkAction className = "tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-blue-500"/>
+		icon: <IconBulkAction className = "tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-blue-500"/>,
+		imageURL: BulkActionsImage,
 	},
 	{
+		id: 6,
 		title: "Backlog & Issue Tracking",
 		description: "Track upcoming work and stay on top of the backlog.",
 		icon: <IconClipboardList className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-red-500" />,
+		imageURL: BacklogImage,
 	},
 	{
+		id: 7,
 		title: "Notifications",
 		description: "Stay informed with smart, real-time notifications.",
 		icon: <IconBell className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-indigo-500" />,
+		imageURL: NotificationsImage,
 	},
 	{
+		id: 8,
 		title: "Organization & User Settings",
 		description: "Manage your organization and personal preferences easily.",
 		icon: <IconGear className="tw-mt-1 tw-shrink-0 tw-h-6 tw-w-6 tw-text-gray-500" />,
+		imageURL: SettingsImage,
 	},
 ];
 
 interface CardProps {
     children: React.ReactNode
     className?: string
+    onClick: () => void
 }
 
-const Card: React.FC<CardProps> = ({ children, className = "" }) => {
+const Card: React.FC<CardProps> = ({ children, className = "", onClick }) => {
     return (
-        <div className={`tw-rounded-2xl tw-shadow-sm tw-border tw-bg-white ${className}`}>
+        <button onClick={onClick} className={`hover:tw-opacity-60 tw-rounded-2xl tw-shadow-sm tw-border tw-bg-white ${className}`}>
             {children}
-        </div>
+        </button>
     )
 }
 
@@ -80,6 +147,7 @@ const CardContent: React.FC<CardContentProps> = ({ children, className = "" }) =
 
 export const LandingPage = () => {
 	const navigate = useNavigate()
+	const [carouselIndex, setCarouselIndex] = useState(0)
 	return (
         <main className="tw-px-6 tw-py-16">
             <div className="tw-max-w-4xl tw-mx-auto tw-text-center">
@@ -90,9 +158,18 @@ export const LandingPage = () => {
                 <button onClick={() => navigate(REGISTER)} className = {`${FADE_ANIMATION} hover:tw-opacity-60 tw-border-gray-300 tw-border tw-inline-block tw-p-2 tw-tw-text-lg tw-text-gray-600 tw-font-bold tw-mb-10`}>Get Started</button>
             </div>
 
+            <MultiCardCarousel index={carouselIndex} items={createImageCarouselElements(features.map((feature: Feature) => {
+				return {
+					id: feature.id,
+					title: feature.title,
+					imageURL: feature.imageURL,
+					description: feature.description
+				}
+			}))} itemsPerPage={1}/>
+
             <div className="tw-grid md:tw-grid-cols-2 tw-gap-6 tw-max-w-5xl tw-mx-auto">
-                {features.map((feature, idx) => (
-                    <Card key={idx} className="tw-p-4">
+                {features.map((feature) => (
+                    <Card key={`feature_${feature.id}`} className="tw-p-4" onClick={() => setCarouselIndex(feature.id-1)}>
                         <CardContent className="tw-flex tw-gap-4 tw-items-start">
                             {feature.icon}
                             <div>
