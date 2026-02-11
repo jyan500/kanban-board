57df1c9ddf462a8a1cd21f120450096eb4762b95
jyan500 <jyan500@gmail.com>
2026-02-10 16:51:42 -0800

    fixed layout shift on the edit ticket modal when switching between comments and work log

--- client/src/components/TicketCommentForm.tsx
+++ client/src/components/TicketCommentForm.tsx
@@ -233,7 +233,7 @@ export const TicketCommentForm = ({currentTicketId, ticketComments}: TicketComme
 					</div>
 				)) : null
 			}
-			<div className = {`${ticketComments.length > 0 ? "tw-min-h-[620px]" : ""} tw-flex tw-flex-col tw-gap-y-4`}>
+			<div className = {`tw-flex tw-flex-col tw-gap-y-4`}>
 				{
 					ticketComments?.map((comment: TicketComment) => (
 						<ProfileActivityRow key={`comment-${comment.id}`} data={comment}>
